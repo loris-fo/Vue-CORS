65bbacbbced95fe4ae438d96dc81d8ad4f736935
lorisfo <loris-fo@outlook.com>
2016-12-22 10:00:51 +0000

    added homepage

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,8 @@
   <nav class="navbar navbar-default">
     <div class="container">
       <ul class="nav navbar-nav">
-        <li><a v-link="'home'">Home</a></li>
+        <li><a v-link="'home'" v-if="!user.authenticated">Home</a></li>
+        <li><a v-link="'homepage'" v-if="user.authenticated">Homepage</a></li>
         <li><a v-link="'login'" v-if="!user.authenticated">Login</a></li>
         <li><a v-link="'login'" v-if="user.authenticated" @click="logout()">Logout</a></li>
       </ul>
